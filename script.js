2f3863658847332b712a5f1178ad8fdd7157e81d
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-03-09 10:46:08 -0300

    atualizado

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ listras.addEventListener("click", abrirMenu);
 function abrirSubMenu(){
     if(this.classList.contains("submenu-active")){
         this.classList.remove("submenu-active");
-    } else {
+    } else if(menu.querySelector(".submenu-active")){
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    }
+    else {
         this.classList.add("submenu-active");
     }
 
@@ -27,6 +31,6 @@ function abrirSubMenu(){
 
 for (let item of itens){
     if (item.querySelector(".submenu")){
-        item.addEventListener("click", abrirDubMenu);
+        item.addEventListener("click", abrirSubMenu);
     }
 }
